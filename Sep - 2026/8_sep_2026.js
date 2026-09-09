6d25045ae1b79c8daffdeb1935f5ed59106ef31c
Abaidullah <hafizabaidullahjutt@gmail.com>
2026-09-08 23:55:06 -0700

    Completed Acronym Builder challenge on 8_sep_2026

---
+++ Sep - 2026/8_sep_2026.js
@@ -0,0 +1,24 @@
+function buildAcronym(str) {
+  const ignore_words = new Set(["a", "for", "an", "and", "by", "of"]);
+  const words = str.split(/\s+/);
+  let result = "";
+
+  for (let i = 0; i < words.length; i++) {
+    const word=words[i];
+    const lowerWord=word.toLowerCase();
+
+   if(i!==0 && ignore_words.has(lowerWord)){
+
+       continue
+    }
+    result+=word[0]
+}
+
+  return result.toUpperCase();
+}
+
+const str = "Federal Bureau of Investigation";
+console.log(buildAcronym(str));
+
+const str_arr = str.split(" ");
+const ignore_letters = ["a", "for", "an", "and", "by", "of"];
